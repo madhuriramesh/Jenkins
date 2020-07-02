4f1b9bd87c16bc85fc8ec1c402cec836d868e780
Madhuri <ma5dhu@gmail.com>
2020-07-03 00:16:09 +0530

    Added test1

---
+++ Test1.py
@@ -0,0 +1,13 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 58BF-E71E
+
+ Directory of C:\Users\DELL\Desktop\Python
+
+07/03/2020  12:15 AM    <DIR>          .
+07/03/2020  12:15 AM    <DIR>          ..
+05/05/2020  11:23 PM                20 Mad.py
+07/02/2020  11:50 PM               447 Test.py
+07/02/2020  11:49 PM               399 Test.txt
+07/03/2020  12:15 AM                 0 Test1.py
+               4 File(s)            866 bytes
+               2 Dir(s)  210,720,571,392 bytes free
